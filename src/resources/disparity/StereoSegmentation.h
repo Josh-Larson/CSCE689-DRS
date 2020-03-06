f63fe16b64ae2742f185692c7316a014206cb070
Josh Larson <joshlarson2015@gmail.com>
2020-03-06 16:05:42 -0500

    Added calculateImageSizeForSegment

--- src/resources/disparity/StereoSegmentation.h
+++ src/resources/disparity/StereoSegmentation.h
@@ -1,1 +1,23 @@
 #pragma once
+
+#include <opencv/cv.h>
+#include <cmath>
+
+cv::Rect2i calculateImageSizeForSegment(const cv::Rect2i& imageSize, const cv::Rect2i& segmentSize, int numDisparities, int blockSize) {
+	assert(imageSize.width > 0 && imageSize.height > 0);
+	assert(segmentSize.x >= 0 && segmentSize.y >= 0);
+	assert(segmentSize.width > 0 && segmentSize.height > 0);
+	assert((segmentSize.x + segmentSize.width) <= imageSize.width);
+	assert((segmentSize.y + segmentSize.height) <= imageSize.height);
+	assert(numDisparities > 0);
+	assert(blockSize > 0);
+	assert(blockSize % 2 == 1);
+	auto halfBlockSize = (blockSize - 1) / 2;
+	auto xStart = std::max(0, segmentSize.x - numDisparities - halfBlockSize);
+	auto yStart = std::max(0, segmentSize.y - halfBlockSize);
+	
+	auto xStartOffset = segmentSize.x - xStart;
+	auto yStartOffset = segmentSize.y - yStart;
+	
+	return cv::Rect2i{xStartOffset, yStartOffset, segmentSize.width, segmentSize.height};
+}
--- test/Josh.cpp
+++ test/Josh.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <resources/disparity/StereoSegmentation.h>
 
 int main(int argc, char * argv[]) {
-	std::cout << "Hello Josh" << std::endl;
+	auto image = cv::Rect2i{0, 0, 2448, 1632};
+	auto numDisparities = 256;
+	auto blockSize = 21;
+	for (int yInd = 0; yInd < 4; yInd++) {
+		for (int xInd = 0; xInd < 4; xInd++) {
+			auto imageSegment = cv::Rect2i{xInd*(image.width/4), yInd*(image.height/4), (image.width/4), (image.height/4)};
+			auto segmentSize = calculateImageSizeForSegment(image, imageSegment, numDisparities, blockSize);
+			fprintf(stdout, "%d-%d  %d-%d    %d-%d  %d-%d\n", imageSegment.x, imageSegment.width, imageSegment.y, imageSegment.height, segmentSize.x, segmentSize.width, segmentSize.y, segmentSize.height);
+		}
+	}
 	return 0;
 }
