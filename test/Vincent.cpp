84be12a1a2bd3df639c2884d40fdad34b4030e18
Vincent Bownes <vincent.bownes@afit.edu>
2020-03-07 16:08:59 -0500

    o Converted stereoBM from python to c++

--- test/Vincent.cpp
+++ test/Vincent.cpp
@@ -62,8 +62,8 @@ int main( int argc, char** argv )
     cv::Mat T = readInMatrix<double, 3, 1>("../GroundTest/perfect_calibrations_4K/T.dat");
 
     // One time operations to fix the stereo images
-    // std::cout << "Initializing StereoBM...\n";
-    cv::Size_<int> imageSize(imgL.rows, imgL.cols);
+     std::cout << "Initializing StereoBM...\n";
+    cv::Size_<int> imageSize(imgL.cols, imgL.rows);
     cv::Mat R1(3, 3, CV_64FC1), R2(3, 3, CV_64FC1), P1(3, 4, CV_64FC1), P2(3, 4, CV_64FC1), Q(4, 4, CV_64FC1);
     cv::stereoRectify(mtxL, distL, mtxR, distR, imageSize, R, T, R1, R2, P1, P2, Q); 
     cv::Mat mapLx, mapLy; 
@@ -75,71 +75,91 @@ int main( int argc, char** argv )
     auto stereo = cv::StereoBM::create(numDisparities, blockSize);
 
     // Fix the individual image single-threaded
-    std::cout << "Fixing input images...";
+    std::cout << "Fixing input images...\n";
     cv::Mat remapL, remapR;
     cv::remap(imgL, remapL, mapLx, mapLy, cv::INTER_LINEAR);
     cv::remap(imgR, remapR, mapRx, mapRy, cv::INTER_LINEAR);
 
     // Testing the algorithm!
-    std::cout <<"Running StereoBM";
+    std::cout <<"Running StereoBM\n";
     cv::Mat disparity, Ndisparity;
     stereo->compute(remapL,remapR, disparity);
     cv::normalize(disparity, Ndisparity, 0, 1, cv::NORM_MINMAX, CV_64FC1);
-    cv::imshow("Disparity", Ndisparity);
+    //cv::imshow("Disparity", Ndisparity);
 
+    std::cout << "Segmenting Image\n";
+    int segments = 4;
+    cv::Size_<int> segmentedSize(imageSize.width / segments, imageSize.height / segments);
+    cv::Mat disparityTest(imageSize.height, imageSize.width, CV_64FC1);  //in yours this is the opposite of image size. why? as in width are height are switched
 
+    std::cout << "Image Size:     " << imageSize << "\n";
+    std::cout << "Segmented Size: " << segmentedSize << "\n";
+    std::cout << "Disparity Size: " << disparityTest.rows << " , " << disparityTest.cols << "\n";
+
+    for(int yInd = 0; yInd < segments; yInd++){
+    	for(int xInd = 0; xInd < segments; xInd++){
+    		//print(str(xInd) + ", " + str(yInd))
+    		int xStart = int(std::max(0, xInd * segmentedSize.width - numDisparities - (blockSize/2)-1)); // blocksize/(2-1) or (blocksize/2)-1
+    		int yStart = int(std::max(0, yInd * segmentedSize.height - (blockSize/2)-1)); //same as above
+    		int xEnd = int(std::min(imageSize.width, (xInd+1) * segmentedSize.width + (blockSize/2)+1)); //same as above
+    		int yEnd = int(std::min(imageSize.height, (yInd+1) * segmentedSize.height + (blockSize/2)+1)); //same as above
+            
+    		int xStartTransfer = int(xInd * segmentedSize.width);
+    		int xEndTransfer = int(xStartTransfer + segmentedSize.width);
+    		int yStartTransfer = int(yInd * segmentedSize.height);
+    		int yEndTransfer = int(yStartTransfer + segmentedSize.height);
+            
+    		int xStartOffset = xStartTransfer - xStart;
+    		int xEndOffset = xEnd - xEndTransfer;
+    		int yStartOffset = yStartTransfer - yStart;
+    		int yEndOffset = yEnd - yEndTransfer;
+            
+    		std::cout << "    "  << xStart << ", " << yStart << "   " << xEnd << ", " << yEnd << "\n";
+            std::cout << "    "  << xStartTransfer << ", " << yStartTransfer << "   " << xEndTransfer << ", " << yEndTransfer << "\n";
+            std::cout << "    "  << xStartOffset << ", " << yStartOffset << "   " << xEndOffset << ", " << yEndOffset << "\n\n";
+
+    		// #print("    " + str(int(len(segment[1])-xStartOffset-xEndOffset)) + ", " + str(int(len(segment[0])-yStartOffset-yEndOffset)))
+            
+            cv::Mat segment;
+    		stereo->compute(remapL.rowRange(yStart,yEnd).colRange(xStart,xEnd), remapR.rowRange(yStart,yEnd).colRange(xStart,xEnd), segment);
+    		//print("    " + str(segment.shape))
+
+            disparityTest.rowRange(yStartTransfer,yEndTransfer).colRange(xStartTransfer,xEndTransfer) = segment.rowRange(yStartOffset, int(segment.rows-yEndOffset)).colRange(xStartOffset, int(segment.cols-xEndOffset));
+        }
+    }
+
+    cv::Mat NdisparityTest , output;
+    cv::normalize(disparityTest, NdisparityTest, 0, 1, cv::NORM_MINMAX, CV_64FC1);
+    
+    cv::bitwise_xor(Ndisparity, NdisparityTest, output);        
+    if(cv::countNonZero(output) > 0){
+        std::cout<< "False";
+    }
+    else{
+        std::cout <<"True";
+    }
+    
+    // for xInd in range(0, segments):
+    // 	cv2.line(disparityTest, (0, int(segmentedSize[1]) * xInd), (imageSize[0], int(segmentedSize[1]) * xInd), 255, 1)
+    // for yInd in range(0, segments):
+    // 	cv2.line(disparityTest, (int(segmentedSize[0]) * yInd, 0), (int(segmentedSize[0]) * yInd, imageSize[1]), 255, 1)
+
+    //cv::imshow("Disparity Test", NdisparityTest)
+    //cv2.imshow('Left', remapL)
+    //cv2.imshow('Right', remapR)
+    //cv2.waitKey()
+    //cv2.destroyAllWindows()
+    
     return 0;
 }
 
 
 
 
-// print("Segmenting Image")
-// segments = 4
-// segmentedSize = (imageSize[0] / segments, imageSize[1] / segments)
-// disparityTest = np.zeros((imageSize[1], imageSize[0]), np.int16)
 
-// print("Image Size:     " + str(imageSize))
-// print("Segmented Size: " + str(segmentedSize))
-// print("Disparity Size: " + str(disparityTest.shape))
 
-// for yInd in range(0, segments):
-// 	for xInd in range(0, segments):
-// 		print(str(xInd) + ", " + str(yInd))
-// 		xStart = int(max(0, xInd * segmentedSize[0] - numDisparities - blockSize/2-1))
-// 		yStart = int(max(0, yInd * segmentedSize[1] - blockSize/2-1))
-// 		xEnd = int(min(imageSize[0], (xInd+1) * segmentedSize[0] + blockSize/2+1))
-// 		yEnd = int(min(imageSize[1], (yInd+1) * segmentedSize[1] + blockSize/2+1))
-		
-// 		xStartTransfer = int(xInd * segmentedSize[0])
-// 		xEndTransfer = int(xStartTransfer + segmentedSize[0])
-// 		yStartTransfer = int(yInd * segmentedSize[1])
-// 		yEndTransfer = int(yStartTransfer + segmentedSize[1])
-		
-// 		xStartOffset = xStartTransfer - xStart
-// 		xEndOffset = xEnd - xEndTransfer
-// 		yStartOffset = yStartTransfer - yStart
-// 		yEndOffset = yEnd - yEndTransfer
-		
-// 		print("    " + str(xStart) + ", " + str(yStart) + "   " + str(xEnd) + ", " + str(yEnd))
-// 		print("    " + str(xStartTransfer) + ", " + str(yStartTransfer) + "   " + str(xEndTransfer) + ", " + str(yEndTransfer))
-// 		print("    " + str(xStartOffset) + ", " + str(yStartOffset) + "   " + str(xEndOffset) + ", " + str(yEndOffset))
-// 		#print("    " + str(int(len(segment[1])-xStartOffset-xEndOffset)) + ", " + str(int(len(segment[0])-yStartOffset-yEndOffset)))
-		
-// 		segment = stereo.compute(remapL[yStart:yEnd, xStart:xEnd],remapR[yStart:yEnd, xStart:xEnd])
-// 		print("    " + str(segment.shape))
-// 		disparityTest[yStartTransfer:yEndTransfer,xStartTransfer:xEndTransfer] = segment[yStartOffset:int(segment.shape[0]-yEndOffset), xStartOffset:int(segment.shape[1]-xEndOffset)]
-		
-// disparityTest = cv2.normalize(disparityTest, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-// for xInd in range(0, segments):
-// 	cv2.line(disparityTest, (0, int(segmentedSize[1]) * xInd), (imageSize[0], int(segmentedSize[1]) * xInd), 255, 1)
-// for yInd in range(0, segments):
-// 	cv2.line(disparityTest, (int(segmentedSize[0]) * yInd, 0), (int(segmentedSize[0]) * yInd, imageSize[1]), 255, 1)
 
-// cv2.imshow('Disparity Test', disparityTest)
-// print(np.array_equal(disparity, disparityTest))
-// cv2.imshow('Left', remapL)
-// cv2.imshow('Right', remapR)
-// cv2.waitKey()
-// cv2.destroyAllWindows()
+
+
+
 
