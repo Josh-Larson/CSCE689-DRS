f37c8138b08163c9c64e3bd2e24b0e490859511a
Josh Larson <joshlarson2015@gmail.com>
2020-03-16 13:11:42 -0400

    Added special constructor to ClusterEndpointId

--- src/resources/networking/cluster/ClusterEndpointId.h
+++ src/resources/networking/cluster/ClusterEndpointId.h
@@ -1,14 +1,17 @@
 #pragma once
 
 #include <string>
+#include <utility>
 
 namespace networking::cluster {
 
 struct ClusterEndpointId {
 	std::string ip;
 	int         port;
-	uint32_t    cpuParalleism;
-	uint32_t    gpuParalleism;
+	uint32_t    cpuParalleism = 0;
+	uint32_t    gpuParalleism = 0;
+	
+	ClusterEndpointId(std::string ip, int port) : ip(std::move(ip)), port(port) {}
 	
 	[[nodiscard]] bool operator==(const ClusterEndpointId &id) const noexcept { return ip == id.ip && port == id.port; }
 	
